d3ed387040baea099bc37930ba99cb1e19feeffa
Bec <bec.braughton@gmail.com>
2017-09-06 13:22:45 -0500

    Happy Birthday Message

---
+++ index.js
@@ -0,0 +1,19 @@
+const chalk = require('chalk');
+//const chalk = require('chalk');
+const log = console.log;
+//console.log(chalk.blue('Hello world!'));
+//log(chalk.blue.bgRed.bold('Hello world!')); Happy Birthday 2 U, Dear Adam 'Goose' Engebretsonnnnn.....
+log(chalk.whiteBright(chalk.underline.bgRed('Happy '), chalk.underline.bgYellow('Birthday '), chalk.underline.bgBlue('2 '), chalk.underline.bgRed('U,')));
+log(chalk.whiteBright(chalk.underline.bgCyan('Happy '), chalk.underline.bgGreen('Birthday '), chalk.underline.bgGreen('2 '), chalk.underline.bgYellow('U,')));
+log(chalk.whiteBright(chalk.underline.bgYellow('Happy '), chalk.underline.bgBlue('Birthday '), chalk.underline.bgMagenta('Dear '), chalk.underline.bgGreen('Adam '), chalk.underline.bgMagenta('"Goose" '), chalk.underline.bgCyan('Engebretsonnnnn..... ')));
+log(chalk.whiteBright(chalk.underline.bgGreen('Happy '), chalk.underline.bgMagenta('Birthday '), chalk.underline.bgRed('ToOooooooo... '), chalk.underline.bgBlue('You!')));
+log(chalk.whiteBright(chalk.underline.bgBlue('..... (¯`v´¯)♥  ')));
+log(chalk.whiteBright(chalk.underline.bgMagenta('.......•.¸.•´  ')));
+log(chalk.whiteBright(chalk.underline.bgRed('....¸.•´  ')));
+log(chalk.whiteBright(chalk.underline.bgYellow('... (  ')));
+log(chalk.whiteBright(chalk.underline.bgGreen(' ☻/  ')));
+log(chalk.whiteBright(chalk.underline.bgBlue('/▌♥♥  ')));
+log(chalk.whiteBright(chalk.underline.bgMagenta('/ \\ ♥♥  ')));
+
+
+
